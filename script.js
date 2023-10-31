cba0f446481cf165ae56fffff4e1bff5a4086b9c
Slionkyiu1129 <130773257+Slionkyiu1129@users.noreply.github.com>
2023-11-01 00:19:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 $(window).mousemove(function (e) {
     $(".circle").css({ top: e.pageY, left: e.pageX }); 
   }); 
+function password(pwd){
+  if(pwd==="9925"){
+    //location.href="https://www.youtube.com/";
+    //location.href="./index1.html";
+    location.href="https://slionkyiu1129.github.io/midterm/D.html";
+    alert("你已破案");
+  }
+  else{
+    alert("ACCESS DENIED!");
+  }
+  }
